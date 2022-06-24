4b7d54f4b08314ef56b0d0690e55f4f1324c26ce
Fahad <fahadshaikh6000@gmail.com>
2022-06-24 22:16:29 +0500

    Make website layout perfectly

--- js/responsive.js
+++ js/responsive.js
@@ -2,9 +2,11 @@ let burger = document.getElementById("burger");
 let navbar = document.getElementById("main-nav");
 let menu = document.getElementById("nav-list");
 let nav_btns = document.querySelector(".btnContainer");
+let resp_logo = document.getElementsByClassName("resp-logo")[0];
 
 burger.addEventListener("click", () => {
   navbar.classList.toggle("main-nav-resp");
   menu.classList.toggle("visible");
   nav_btns.classList.toggle("visible");
+  resp_logo.classList.toggle("ds-none");
 });
