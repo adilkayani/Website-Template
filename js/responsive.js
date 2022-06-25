82a6f9b93d75abc555949a204d68d901719496d4
Fahad <fahadshaikh6000@gmail.com>
2022-06-26 01:38:13 +0500

    Configurate the files

--- js/responsive.js
+++ js/responsive.js
@@ -1,10 +1,9 @@
-let burger = document.getElementById("burger");
-let navbar = document.getElementById("main-nav");
-let menu = document.getElementById("nav-list");
-let nav_btns = document.querySelector(".btnContainer");
-let resp_logo = document.getElementsByClassName("resp-logo")[0];
-
 burger.addEventListener("click", () => {
+  let burger = document.getElementById("burger");
+  let navbar = document.getElementById("main-nav");
+  let menu = document.getElementById("nav-list");
+  let nav_btns = document.querySelector(".btnContainer");
+  let resp_logo = document.getElementsByClassName("resp-logo")[0];
   navbar.classList.toggle("main-nav-resp");
   menu.classList.toggle("visible");
   nav_btns.classList.toggle("visible");
